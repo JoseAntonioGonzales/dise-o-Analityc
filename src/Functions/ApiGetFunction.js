a8f753552d563697748602c13dc718937f92ec8e
PabloMunozVillegas <p.munoz@opti-system.com>
2024-07-16 15:11:26 -0400

    asdasd

--- src/Functions/ApiGetFunction.js
+++ src/Functions/ApiGetFunction.js
@@ -31,6 +31,7 @@ const ApiGetFunction = async (urlKey, params = {}) => {
         }
 
         const response = await axios.get(url);
+        console.log(response.data);
         return response.data;
     } catch (error) {
         console.error(`Error en ApiGetFunction para ${urlKey}:`, error);
