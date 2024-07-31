c3617712010e8e30009045fd0ed59e2fca69491d
JoseAntonioGonzales <j.gonzales@opti-system.com>
2024-07-31 17:29:32 -0400

    traficoEnRojo

--- src/cubitosCOmpa/index.js
+++ src/cubitosCOmpa/index.js
@@ -12,9 +12,12 @@ function CubitosCOmpa() {
                     "TKT PROMEDIO",
                     "TRAFICO"
                 ].map((title, index) => (
-                    <div key={index} className="p-3 border rounded-lg bg-white shadow-sm flex flex-col items-center justify-center">
-                        <h3 className="text-xs font-bold text-center text-gray-800 mb-2">{title}</h3>
-                        <span className="text-lg font-semibold text-blue-600">0</span>
+                    <div
+                        key={index}
+                        className={`p-3 border rounded-lg shadow-sm flex flex-col items-center justify-center ${title === "TRAFICO" ? 'bg-red-500 text-white' : 'bg-white text-gray-800'}`}
+                    >
+                        <h3 className="text-xs font-bold text-center mb-2">{title}</h3>
+                        <span className={`text-lg font-semibold ${title === "TRAFICO" ? 'text-white' : 'text-blue-600'}`}>0</span>
                     </div>
                 ))}
             </div>
@@ -27,9 +30,12 @@ function CubitosCOmpa() {
                     "TOTAL VENTA",
                     "CANTIDAD SUCURSALES"
                 ].map((title, index) => (
-                    <div key={index} className="p-3 border rounded-lg bg-white shadow-sm flex flex-col items-center justify-center">
-                        <h3 className="text-xs font-bold text-center text-gray-800 mb-2">{title}</h3>
-                        <span className="text-lg font-semibold text-blue-600">0</span>
+                    <div
+                        key={index}
+                        className={`p-3 border rounded-lg shadow-sm flex flex-col items-center justify-center ${title === "TRAFICO" ? 'bg-red-500 text-white' : 'bg-white text-gray-800'}`}
+                    >
+                        <h3 className="text-xs font-bold text-center mb-2">{title}</h3>
+                        <span className={`text-lg font-semibold ${title === "TRAFICO" ? 'text-white' : 'text-blue-600'}`}>0</span>
                     </div>
                 ))}
             </div>
